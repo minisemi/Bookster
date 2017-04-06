583f4d4362345abd3c99a514991a43d38fd1c608
minisemi <a.ulander@live.se>
2017-04-06 16:29:42 +0200

    Added watchers and babel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
+
+var dropdown =
+  <Dropdown>
+    A dropdown list
+    <Menu>
+      <MenuItem>Do Something</MenuItem>
+      <MenuItem>Do Something Fun!</MenuItem>
+      <MenuItem>Do Something Else</MenuItem>
+    </Menu>
+  </Dropdown>;
+
+  render(dropdown)
 
 class App extends Component {
   render() {
@@ -11,7 +29,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and SAVE to reload.
         </p>
       </div>
     );
