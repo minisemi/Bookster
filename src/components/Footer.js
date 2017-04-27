4e59f951097b40397d8924086b690d6120fe6d72
minisemi <a.ulander@live.se>
2017-04-27 17:40:21 +0200

    Fixed responsive design on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,10 +11,22 @@ class Footer extends Component {
 
 
       <footer >
-          <Col xs={12} sm={12} md={3} lg={2}>
+          <Col  xs={3} smOffset={1} sm={3} mdOffset={1} md={3} lgOffset={1} lg={3}>
             <div>
-          <h2 className="footer-header">Bookster</h2>
+          <h2 className="header">Bookster</h2>
             </div></Col>
+          <Col xsOffset={2} xs={3} smOffset={2} sm={3} mdOffset={2} md={3} lgOffset={1} lg={3} className="contactInfo">
+
+              <row>
+                  <p className="contactInfo topRow"><b>Contact information</b></p>
+              </row>
+              <row>
+                  <p className="contactInfo">Email: info@bookster.se</p>
+              </row>
+              <row>
+                  <p className="contactInfo">Address: Studievägen 9B lgh 922070</p>
+              </row>
+          </Col>
         </footer>
 
     );
