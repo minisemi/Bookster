f6a13f37d87131487183739fec5b2180cee6e4c3
minisemi <a.ulander@live.se>
2017-05-04 12:28:35 +0200

    Moved searchbar to header

--- src/components/App.js
+++ src/components/App.js
@@ -14,11 +14,7 @@ export default class App extends Component {
     return (
 
       <div className="App">
-
           <Grid>
-              <Row>
-                <BookingsSearch/>
-              </Row>
 
           <Row>
               <BookingsSlideBar title="My bookings"/>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 import '../static/Nav.css';
 import LogInForm from './loginPage/LogInForm';
 import {Row, Col} from 'react-bootstrap';
+import BookingsSearch from './BookingsSearch';
 
 
 class Nav extends Component {
@@ -11,23 +12,18 @@ class Nav extends Component {
     return (
 
         <div >
-          <Row className="noMargin">
             <header>
               <Link to={"/"}>
-                <Col xs={12} sm={12} md={3} lg={2}>
                 <div className="navbar-header">
                   <h1>Bookster</h1>
                 </div>
-                </Col>
               </Link>
-              <Col xs={12} sm={12} mdOffset={1} md={8} lgOffset={3} lg={7}>
+              <BookingsSearch/>
                 <ul className="nav navbar-nav navbar-right">
                   <LogInForm/>
                 </ul>
-              </Col>
             </header>
 
-          </Row>
 
         </div>
 
